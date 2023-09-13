a6800ecbb46b937eb8238e8346855e126b6d47d2
wamuM <el.mail.de.wamu.m@gmail.com>
2023-09-14 01:29:45 +0200

    Added error handling to regex

--- scripts/new.sh
+++ scripts/new.sh
@@ -2,8 +2,13 @@ echo "Jutge Problem URI (i.e P92613, https://jutge.org/problems/P92613_en, etc)"
 read link;
 # Parse text (I know this is awful but I couldn't figure out a more universal way)
 regex="((http)?s?(://)?jutge.org/problems/)?([A-Z]\d{5})_(\w\w)";
-grep -Eo $regex>"./.match.jutge"
+echo $link | grep -Eo $regex>"./.match.jutge"
 tail -n +3 "./.match.jutge"
+if [-z "./.match.jutge"];
+  do
+    echo "Error: Provided text wasn't a valid jutge.org problem"
+    exit;
+  fi
 {read name; read language}<"./.match.jutge"
   
 link="https://jutge.org/problems/$name_$language";
