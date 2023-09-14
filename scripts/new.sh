93ba69a4c2267f3ec9449363a63223fbb44b4928
wamuM <el.mail.de.wamu.m@gmail.com>
2023-09-14 03:23:43 +0200

    v1.0.0
    This is just to prove that I know how to write proper commit messages
    but the pain of writing this at almost 4am made my last commits quite
    unprofessional...

--- scripts/new.sh
+++ scripts/new.sh
@@ -19,16 +19,17 @@ cd "./$name" || exit 1;
 echo "Downloading files from jutge..."
 wget -O "jutge.zip" "${link}/zip"
 unzip "jutge.zip" -d .
+rm "jutge.zip"
 mv "./${name}_${language}" "./samples"
-cp "../.jutge/check-samples.sh" "./check-samples.sh"
+cp "../../.jutge/check-samples.sh" "./check-samples.sh"
 mv "./samples/problem.pdf" "./problem.pdf"
 mv "./samples/problem.ps" "./problem.ps"
-cp "../jutge/template-main.cpp" "./main.cpp"
-if test -d "../.jutge/use-git";
+cp "../../.jutge/template-main.cpp" "./main.cpp"
+if test -d "../../.jutge/use-git";
 then
-  echo Git: Initiating Git repository 
+  echo "Git: Initiating Git repository..." 
   git init -q
   git add * -q 
-  echo Git: Commiting initial state
+  echo "Git: Commiting initial state..."
   git commit -q -m "Automatic First Commit by the Jutge Manager Script"
 fi 
