dddadb10912b75ccd6091bbc4a967bc489cb8a3e
wamuM <el.mail.de.wamu.m@gmail.com>
2023-09-14 02:30:44 +0200

    please work

--- scripts/new.sh
+++ scripts/new.sh
@@ -1,23 +1,27 @@
 echo "Jutge Problem URI (i.e P92613, https://jutge.org/problems/P92613_en, etc)";
 read link;
 # Parse text (I know this is awful but I couldn't figure out a more universal way)
-regex="((http)?s?(://)?jutge.org/problems/)?([A-Z]\d{5})_(\w\w)";
+regex='((http)?s?(://)?jutge.org/problems/)?([:upper:][:digit:]{5})_([:lower:][:lower:])';
 echo $link | grep -Eo $regex>"./.match.jutge"
 tail -n +3 "./.match.jutge";
-if test -z ${cat "./.match.jutge"});
-then
-    echo "Error: Provided text wasn't a valid jutge.org problem"
-    exit;
+if [[ $link=~ $regex]]; 
+then 
+  name=$(BASH_REMATCH[4]);
+  language=$(BASH_REMATCH[5]);
+else
+    echo "Error: Provided text wasn't a valid jutge.org problem";
+    exit 1;
 fi
 {read name; read language}<${"./.match.jutge"};
 rm "./.match.jutge" 
-link="https://jutge.org/problems/$name_$language";
+link="https://jutge.org/problems/${name}_${language}";
 echo "Creating files for '$link'"
 
 # Creating files 
-mkdir "./$name"
-cd "./$name"
-wget -O "jutge.zip" $link 
+mkdir "./$name" || exit 1;
+cd "./$name" || exit 1;
+echo "Downloading files from jutge..."
+wget -O "jutge.zip" $link
 unzip "jutge.zip" -d ./samples
 cp "../.jutge/check-samples.sh" "./check-samples.sh"
 mv "./samples/problem.pdf" "./problem.pdf"
