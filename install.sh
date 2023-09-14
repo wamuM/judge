18fd1e433b2c6cdbf5d63fc7f90ce37babd81db4
wamuM <el.mail.de.wamu.m@gmail.com>
2023-09-14 21:34:24 +0200

    Added Better Config
    After some testing from some of my classmates the configuration
    questions where proven to be too confusing, so now you can just skip
    them and easily stick to defaults. Plus since what people understood
    less was Parts, I added the add.sh file to help them create more if
    the default wasn't enough.

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
+#!/bin/bash 
+
+function fetchConfig(){
+  test -f "./.jutge/$1" && return 0 ||  return 1;
+}
+function toggleFile {
+  # $toggleFile_file
+  if fetchConfig $1
+  then
+    rm "./.jutge/$1"
+  else 
+    touch "./.jutge/$1"
+  fi
+  
+}
+
 echo "================================="
 echo ""
 echo "     W e l c o m e    T o"
@@ -6,36 +22,54 @@ echo " J u t g e    E n v i r o m e n t"
 echo "      I n s t a l l e r"
 echo ""
 echo "================================="
-echo CC-BY-SA wamu_M 2023
-# Create PN folders
-echo "ID of the last part (i.e for P0,P1,P2,P3,P4 it would be 4)"
-read parts; 
+echo "CC-BY-SA wamu_M 2023"
+
+shouldInstall="no";
+parts=1;
+
+while true;
+do
+echo ""
+echo "Select an option:"
+shouldGit=$(fetchConfig "use-git" && echo "Yes" || echo "No (Default)");
+shouldClear=$(fetchConfig "clear-install" && echo "Yes (Default)" || echo "No");
+shouldVerbose=$(fetchConfig "verbose" && echo "Yes (Default)" || echo "No");
+echo "g) Automatically initialize Git in Problem Folders? ${shouldGit}"; 
+echo "c) Clear installation files when done? ${shouldClear}"; 
+echo "v) Have unzip and wget be verbose? ${shouldVerbose}"; 
+echo "p) Number of parts: ${parts}";
+echo "i) [install] Proceed with installation";
+read -p "#? " config
+case $config in 
+  "g") toggleFile "use-git";
+    ;;
+  "c") toggleFile "clear-install";
+    ;;
+  "v") toggleFile "verbose";
+    ;;
+  "p") read -p "How many parts do you want? " parts;
+    ;;
+  "i") break;
+    ;;
+  *)
+    echo "Please select a valid config option:";
+    ;;
+esac; 
+done;
+echo ""
+echo "Starting installation..."
+mv "./.jutge/add.sh" "./add.sh"
 i=0;
 while test $i -le $parts;
 do 
    mkdir "P$i"
-   cp "./scripts/new.sh" "./P$i/new.sh"
+   cp "./.jutge/new.sh" "./P$i/new.sh"
    let "i++"
 done
-# Creating directory 
-mkdir .jutge
-cp "./scripts/check-samples.sh" "./.jutge/check-samples.sh"
-cp "./scripts/template-main.cpp" "./.jutge/template-main.cpp"
-# Settings 
-read -p "Do you want to automatically init git in every problem (recommended)?[Y/n]" shouldGit
-case $shouldGit in 
-  [Nn]* ) rem; break;;
-  * ) touch "./.jutge/use-git";;
-esac 
-# Clear afterwards
-read -p "Do you want to clear the installation files (recomended)[Y/n]?" shouldClear
-case $shouldClear in 
-  [Nn]* ) exit; break;;
-  * ) echo "Deleting installation files...";;
-esac 
-rm -r ./scripts/ 
-rm init.sh 
-rm -rf "./.git" 
-rm README.md 
-rm LICENSE
-echo Installation Completed!
+
+if fetchConfig "clear-install";
+then 
+echo "[+] Installation files cleared"
+rm -r install.sh LICENSE README.md "./.jutge/clear-install" 
+fi 
+echo "[+] Installation Completed!"
